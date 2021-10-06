8abe294cc755dc3ffe77da9b56bbfe67f575d4df
emylincon <emylincon@gmail.com>
2021-10-06 21:15:36 +0100

    moving average added

--- main.py
+++ main.py
@@ -72,13 +72,13 @@ def plot_temp_graph():
     fig1 = plt.ion()
     # fig1 = plt.grid(True, color='k')
     #fig1 = plt.scatter(table, list(range(0,len(table))), label='Temp C')
-    fig1 = plt.plot(table, ':o', label='Time Series')
+    fig1 = plt.plot(table, ':o', label=f'Current Average = {round(sum(table)/len(table),3)}')
     fig1 = plt.title('Telemetry Monitor')
     fig1 = plt.ylabel('Data Value')
     fig1 = plt.xlabel('Time (seconds)')
     fig1 = plt.grid(True)
     fig1 = plt.legend()
-    fig1 = plt.pause(2)
+    #fig1 = plt.pause(1)
 
 
 def main():
